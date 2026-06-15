df3b3c5a51bd44160960fb355f722ac9a43eef46
Ubuntu <azureadmin@devops-vm.j0gkzknweu2eho3tx3lp0pmetb.rx.internal.cloudapp.net>
2026-06-15 11:38:53 +0000

    Initial Azure DevOps platform project

---
+++ terraform/variables.tf
@@ -0,0 +1,15 @@
+variable "subscription_id" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "acr_name" {
+  type = string
+}
