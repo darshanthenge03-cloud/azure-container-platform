1deb7c3908a1279c958e0f9700a4dcc21f6a1205
Darshan Thenge <darshanthenge03@gmail.com>
2026-06-22 00:59:28 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,14 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Azure DevOps Platform Project Version 2 deployed via GitHub Actions" 
+    return render_template("index.html")
 
 @app.route("/health")
 def health():
-    return "Healthy"
+    return {
+        "status": "Healthy",
+        "environment": "Production"
+    }
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
